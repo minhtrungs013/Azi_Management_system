686c1af72fcb550090caa739c6e806734c0491d9
minhtrungs013 <trungs0132@gmail.com>
2024-12-18 22:44:31 +0700

    add fix #13

--- app/home/[homeId]/page.tsx
+++ app/home/[homeId]/page.tsx
@@ -1,34 +1,24 @@
-'use client';
-import ProjectHeader from '@/components/project/projectHeader/projectHeader';
-import ProjectTodo from '@/components/project/projectTodo/projectTodo';
-import { getProjectId } from '@/lib/store/features/projectSlice';
-import { AppDispatch, RootState } from '@/lib/store/store';
-import { ProjectDetails } from '@/types/project';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 
-export default function ProjjectId({ params }: { params: { projjectId: string } }) {
-    const dispatch = useDispatch<AppDispatch>();
-    const [data, setData] = useState<ProjectDetails>()
-    const refresh = useSelector((state: RootState) => state.task.refresh);
 
-    useEffect(() => {
-        (async () => {
-            const res = await dispatch(getProjectId(params.projjectId));
-            if (getProjectId.fulfilled.match(res)) {
-                setData(res.payload);
-            }
-        })();
-    }, [refresh, params.projjectId])
+export default function HomeId({ params }: { params: { homeId: string } }) {
+    console.log('aaa');
+    
+    // const dispatch = useDispatch<AppDispatch>();
+    // const [data, setData] = useState<ProjectDetails>()
+    // const refresh = useSelector((state: RootState) => state.task.refresh);
+
+    // useEffect(() => {
+    //     (async () => {
+    //         const res = await dispatch(getProjectId(params.homeId));
+    //         if (getProjectId.fulfilled.match(res)) {
+    //             setData(res.payload);
+    //         }
+    //     })();
+    // }, [refresh, params.homeId])
 
     return (
         <div >
-            <div className="flex ">
-                <main className="flex-1 p-6 ">
-                    <ProjectHeader data={data} />
-                    <ProjectTodo data={data} />
-                </main>
-            </div>
+           hihihi
         </div>
     );
 }
